a685adb1ce1c395d4e90255002657b1d823457cb
James Reidy <jreidy@cornell.edu>
2017-02-09 15:06:45 -0500

    insert the standard pantheon settings.php

--- d8.settings.php
+++ d8.settings.php
@@ -29,6 +29,42 @@ if (isset($_SERVER['PANTHEON_ENVIRONMENT']) &&
 }
 
 /**
+ * Contents of standard Pantheon Drupal 8 settings.php
+ * VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+ */
+
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+/**
+ * Include the Pantheon-specific settings file.
+ *
+ * n.b. The settings.pantheon.php file makes some changes
+ *      that affect all envrionments that this site
+ *      exists in.  Always include this file, even in
+ *      a local development environment, to insure that
+ *      the site settings remain consistent.
+ */
+include __DIR__ . "/settings.pantheon.php";
+
+/**
+ * If there is a local settings file, then include it
+ */
+$local_settings = __DIR__ . "/settings.local.php";
+if (file_exists($local_settings)) {
+  include $local_settings;
+}
+$settings['install_profile'] = 'standard';
+
+/**
+ * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * end of standard Pantheon Drupal 8 settings.php
+ */
+
+
+/**
  * PHP settings:
  *
  * To see what PHP settings are possible, including whether they can be set at
